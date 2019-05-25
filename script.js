9d172eaa545fb08347108703d001f6d8a060c79d
NycollSM <nycollsotomorales21@gmail.com>
2019-05-25 12:27:02 -0600

    deleting things that are doing anythig

--- script.js
+++ script.js
@@ -7,48 +7,15 @@
     //disable button
     createBtn.setAttribute('disabled', true);
     //elements
-    const dialog = document.createElement('dialog');
-    dialog.setAttribute('open', open);
-    const form = document.createElement('form');
+    const title= document.getElementById('inputTile');
+    const description = document.getElementById('description');
 
-    const titleLabel = document.createElement('label');
-    titleLabel.innerText = 'Title';
-
-    const inputTitle = document.createElement('input');
-    inputTitle.setAttribute('type', 'text');
-    inputTitle.setAttribute('maxlength', 25);
-
-    const content = document.createElement('label');
-    content.innerText = 'Description';
-
-    const contentInput = document.createElement('input');
-    contentInput.setAttribute('type', 'text');
-
-    //buttons
-    const saveBtn = document.createElement('button');
-    saveBtn.innerText = 'Save';
-    const deleteBtn = document.createElement('button');
-    deleteBtn.innerText = 'Delete';
-    const closeBtn = document.createElement('button');
-    closeBtn.innerText = 'X';
-
-    //add to dom
-    const container = document.getElementById('mainContainer');
-    form.appendChild(titleLabel);
-    form.appendChild(inputTitle);
-    form.appendChild(content);
-    form.appendChild(contentInput);
-    form.appendChild(saveBtn);
-    form.appendChild(deleteBtn);
-    form.appendChild(closeBtn);
-    dialog.appendChild(form);
-    container.appendChild(dialog);
 
     function getValues (){
       event.preventDefault();
       let Note = {
-        title : inputTitle.value,
-        description : contentInput.value,
+        title : title.value,
+        description : description.value,
         date : new Date(), 
         id : null
       }
